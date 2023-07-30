3368f90d4c3be9aad7a90196c4c2920be97db163
Akash Bag <110753356+akash0708@users.noreply.github.com>
2023-07-30 13:23:24 +0530

    completed upto coming soon

--- app/page.js
+++ app/page.js
@@ -133,6 +133,60 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="feature_extended w-full h-fit bg-gradient-to-l from-[#23040E] via-[#3b1023]">
+        <div className="w-full sm:w-4/5 h-[40rem] relative">
+          <Image
+            src="/featextd.png"
+            alt="feature_extended"
+            fill
+            className="object-cover mix-blend-lighten"
+          />
+          <div className="heading absolute w-[24rem] sm:right-[-5rem] top-10 font-Raleway font-semibold uppercase text-[#DBCEB5] text-center text-3xl sm:text-5xl leading-[2.75rem] sm:leading-[4rem] tracking-wider">
+            ELEVATE YOUR OCCASSIONS WITH PEGMAN
+          </div>
+          <div className="absolute w-fit right-12 sm:right-[-4.5rem] bottom-[15rem] sm:bottom-[12.5rem] flex flex-row">
+            <ul className="list-disc list-outside marker:text-[#AD133C] text-white font-Raleway text-3xl">
+              <li className="leading-10">Corporate events</li>
+              <li className="leading-10">Birthdays</li>
+              <li className="leading-10">Anniversaries</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="coming-soon flex flex-col items-center w-full min-h-screen bg-gradient-to-tr from-[#2d0c1b] via-[#3b1023] to-[#720A30]">
+        <div className="flex flex-row items-center pt-4">
+          <div className="bg-gradient-to-l from-[#BF1B42] w-16 sm:w-48 h-1 rounded-md mt-5"></div>
+          <span className="text-[#CDBC9E] font-Prociono text-[2.5rem] sm:text-[5rem] pt-4 px-2 sm:px-10">
+            Coming Soon
+          </span>
+          <div className="bg-gradient-to-r from-[#BF1B42] w-16 sm:w-48 h-1 rounded-md mt-5"></div>
+        </div>
+        <div className="content relative w-full sm:min-h-fit flex flex-col sm:flex-row justify-between py-12">
+          <div className="image_container relative w-4/5 sm:w-64 h-[37rem] sm:right-[-14rem]">
+            <Image
+              src="/phone.png"
+              alt="phone"
+              fill
+              className="object-fit ml-[2.5rem]"
+            />
+          </div>
+          <div className="content_container relative text-left w-96 sm:w-[28.5rem] sm:h-80 top-20 sm:left-[-23rem] font-Raleway px-8">
+            <p className="heading uppercase text-[#F4E9ED] font-semibold text-3xl leading-[3rem] mb-16">
+              PEGS NOW AT YOUR FINGERTIPS
+            </p>
+            <p className="content text-[#F9D7D7]">
+              Get ready for a revolutionary way to elevate your alcohol delivery
+              experience with
+              <span className="font-semibold">Pegman in Kolkata!</span> <br />
+              <br /> Our user-friendly app will provide a delightful browsing
+              experience, allowing you to explore an extensive collection of
+              beverages from renowned brands and hidden gems alike.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "feature-extended": "url('/featextd.png')",
       },
       fontFamily: {
         Raleway: ["Raleway", "sans-serif"],
